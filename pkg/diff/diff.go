66d0109710bf5a6aa4b866821b9c979c0c33548e
Rajath Agasthya <ragasthya@vmware.com>
2022-01-25 15:20:40 -0600

    Make some changes

--- pkg/diff/diff.go
+++ pkg/diff/diff.go
@@ -5,10 +5,6 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func SliceDiff(a, b []string) string {
-	return cmp.Diff(a, b, cmpopts.EquateEmpty(), cmpopts.SortSlices(func(x, y string) bool { return x < y }))
-}
-
 func SliceDiffIgnoreOrder(a, b []string) string {
 	return cmp.Diff(a, b, cmpopts.EquateEmpty(), cmpopts.SortSlices(func(x, y string) bool { return x < y }))
 }
